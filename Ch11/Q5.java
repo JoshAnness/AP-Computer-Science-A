da58fcc94cea6de5ca3901a7fc58ee47c15c09d5
Josh Anness <annessjosh@gmail.com>
2020-02-11 10:28:34 -0500

    Chapter 11

---
+++ Ch11/Q5.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Q5 {
+   public static ArrayList<String> reverse(ArrayList<String> arr) {
+      ArrayList<String> result = new ArrayList<String>();
+      
+      for(int i = arr.size()-1; i > -1; i--) {
+         result.add(arr.get(i));
+      } 
+      
+      return result;
+   }
+   
+   public static void main(String[] args) {      
+      ArrayList<String> data = new ArrayList<String>();
+      
+      data.add("Josh");
+      data.add("Ben");
+      data.add("Alex");
+      data.add("David");
+      data.add("Joel");   
+      
+      System.out.print(reverse(data));
+   }
+}
---
+++ Ch11/Q6.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Q6 {
+   public static ArrayList<Integer> smallestValue(ArrayList<Integer> arr) {     
+      int index = 0;
+      
+      for(int i = 1; i < arr.size(); i++) {
+         if(arr.get(index).compareTo(arr.get(i)) > 0)
+            index = i;
+      } 
+      
+      int num = arr.get(index);
+      
+      for(int i = 0; i < arr.size(); i++) {
+         if(arr.get(i).compareTo(num) == 0)
+            arr.remove(i);
+      }
+      
+      return arr;
+   }
+   
+   public static void main(String[] args) {      
+      ArrayList<Integer> data = new ArrayList<Integer>();
+      
+      data.add(1);
+      data.add(-3);
+      data.add(2);
+      data.add(-3);
+      data.add(2);   
+      
+      System.out.print(smallestValue(data));
+   }
+}
