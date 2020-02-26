e1f27dbe2867fa28fbf18d29a4962f09ba5d66e1
Josh Anness <annessjosh@gmail.com>
2020-02-25 22:42:56 -0500

    Update

--- Ch12/Exercises/Q6/Main.java
+++ Ch12/Exercises/Q6/Main.java
@@ -3,14 +3,15 @@ import java.util.*;
 public class Main {
    
    public static void main(String[] args) {
-      EquilateralTriangle tri1 = new EquilateralTriangle(2);
+      EquilateralTriangle tri1 = new EquilateralTriangle(3);
       
-      RightTriangle tri2 = new RightTriangle(2);
+      RightTriangle tri2 = new RightTriangle(3);
       
-      System.out.println("Triangle 1 Area: " + tri1.getArea() + ", Triangle 1 Perimeter: " + tri1.getPerimeter());
+      System.out.println("Triangle 1, Area: " + tri1.getArea() + ", Perimeter: " + tri1.getPerimeter());
       
-      System.out.println("Triangle 2 Area: " + tri2.getArea() + ", Triangle 2 Perimeter: " + tri2.getPerimeter());
+      System.out.println("Triangle 2, Area: " + tri2.getArea() + ", Perimeter: " + tri2.getPerimeter());
    
+      System.out.print("Q: The right triangle can hold a bigger circle inside");
    }
 
 }
