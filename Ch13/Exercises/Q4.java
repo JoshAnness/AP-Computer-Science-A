bafd7b1e82f140865ffdee34cfc34d276f0b933a
Josh Anness <annessjosh@gmail.com>
2020-04-07 13:31:42 -0400

    Q4

--- Ch13/Exercises/Q4.java
+++ Ch13/Exercises/Q4.java
@@ -2,13 +2,25 @@ import java.util.*;
 
 public class Q4 {
    public static int findMin(int[] list, int n) {
+      n--;
       
-   
+      if(n == 0)
+         return list[0];
+         
+      else {
+         if(list[n] > list[n-1])
+            return findMin(list, n);
+         else {
+            list[n-1] = list[n];
+            return findMin(list, n);
+         }
+      }  
+       
    }
 
    public static void main(String args[]) {
       int[] listOfNumbers = {6, 5, 3, 11, 17, 2, 8, 7, 6};
-      int numberOfItemsInListToExamine = 6;
+      int numberOfItemsInListToExamine = 9;
       
       System.out.println(findMin(listOfNumbers, numberOfItemsInListToExamine));
    }
